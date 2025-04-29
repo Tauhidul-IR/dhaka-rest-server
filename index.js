f02e803f70b2a9767829f42a654b935dc85edae4
Robin <robin13690@gmail.com>
2025-04-29 07:39:59 +0600

    admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -43,6 +44,38 @@ async function run() {
     const result = await userCollection.insertOne(user);
     res.send(result);
   });
+
+// get users 
+app.get('/users', async (req, res) => {
+  // console.log(req.headers);
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete user 
+app.delete('/users/:id',  async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
+})
+
+
+//update user role
+app.patch('/users/admin/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      role: 'admin'
+    }
+  }
+  const result = await userCollection.updateOne(filter, updatedDoc);
+  res.send(result);
+})
+
+
+
   //------------------------------User-----------------------
 
 
